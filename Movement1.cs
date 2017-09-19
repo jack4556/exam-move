c7b0f820993670a09599384304296b93e3031758
jack4556 <jackdmcpherson455@gmail.com>
2017-09-19 17:31:14 +1000

    Add files via upload
    jack exam 
    movement and jump

---
+++ Movement1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey(KeyCode.RightArrow)){
+			transform.position += transform.right;
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)){
+			transform.position -= transform.right; 
+		}
+		if (Input.GetKey(KeyCode.UpArrow)){
+			transform.position += transform.forward;
+		}
+		if (Input.GetKey(KeyCode.DownArrow)){
+			transform.position -= transform.forward;
+		}
+	}
+}
